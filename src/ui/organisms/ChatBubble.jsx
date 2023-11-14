76804742d5060ead284d60e84ac0e613b8a50546
Myrtyl Sri Wahyuni Santos <myrtyl@solfin.co.id>
2023-11-14 23:22:10 +0700

    Create Chat Bubble template

---
+++ src/ui/organisms/ChatBubble.jsx
@@ -0,0 +1,23 @@
+import Grid from '@mui/material/Grid';
+import {TextMessage} from '../molecules/TextMessage';
+import { Avatar, Paper } from '@mui/material';
+
+export const ChatBubble = ({self}) => (
+  <Paper
+    sx={{
+      my: 1,
+      mx: 'auto',
+      p: 2,
+    }}
+  >
+    <Grid container wrap="nowrap" spacing={2} direction={self ? 'row-reverse' : 'row'}>
+      <Grid item>
+        {!self && <Avatar>W</Avatar>}
+      </Grid>
+      <Grid item xs>
+      <TextMessage self/>
+
+      </Grid>
+    </Grid>
+  </Paper>
+)
