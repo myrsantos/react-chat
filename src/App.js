5562eeb0a891410155dffc15974eb5349e1bcd76
Myrtyl Sri Wahyuni Santos <myrtyl@solfin.co.id>
2023-11-14 23:23:35 +0700

    Template for basic chat app done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
+import {ChatContainer} from "./ui/organisms/ChatContainer";
 import { Container } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <Container maxWidth="sm" >
-
+        <ChatContainer/>
       </Container>
     </div>
   );
